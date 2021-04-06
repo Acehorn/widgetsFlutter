d997ab5ff9a393c7301ff2d7888069cdffd1531f
Luis Castillo <53038393+Acehorn@users.noreply.github.com>
2021-04-06 18:19:39 -0500

    Update constantes.dart

--- constantes.dart
+++ constantes.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+
+var blueColorGNP =  Color.fromRGBO(51, 68, 95, 1);
+var naranjaColorGNP = Color.fromRGBO(255, 107, 11, 1);
+var greyColorGNP = Color.fromRGBO(206, 216, 232, 1);
+
 var currencyMXN = NumberFormat.currency(locale: "es_MX", decimalDigits: 2, name: "" , symbol: "\u0024");
