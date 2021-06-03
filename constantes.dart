6ffb01e12d83431638cfad7d419c46a5ebb696b7
Luis Castillo <53038393+Acehorn@users.noreply.github.com>
2021-06-02 22:31:12 -0500

    Update constantes.dart

--- constantes.dart
+++ constantes.dart
@@ -7,3 +7,9 @@ var naranjaColorGNP = Color.fromRGBO(255, 107, 11, 1);
 var greyColorGNP = Color.fromRGBO(206, 216, 232, 1);
 
 var currencyMXN = NumberFormat.currency(locale: "es_MX", decimalDigits: 2, name: "" , symbol: "\u0024");
+
+
+
+//REGEX
+
+String email="^[a-zA-Z0-9.!#'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*";
