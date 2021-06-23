f8df5eed3e1d74618a1a2b19cc9c6f57bdd11d30
Luis Castillo <53038393+Acehorn@users.noreply.github.com>
2021-06-23 01:25:53 -0500

    Update dropDown.dart

--- dropDown.dart
+++ dropDown.dart
@@ -6,24 +6,25 @@ class DropDownRegister extends StatefulWidget {
   final String opcion2;
   final String opcion3;
   final String opcion4;
+  final onChanged;
 
 
-  const DropDownRegister({ Key key, @required this.titleDropHint,@required this.opcion1,@required this.opcion2, @required this.opcion3,@required this.opcion4,  }) : super(key: key);
+  const DropDownRegister({ Key key, @required this.titleDropHint,@required this.opcion1,@required this.opcion2, @required this.opcion3,@required this.opcion4, @required this.onChanged,   }) : super(key: key);
 
   @override
   _DropDownRegisterState createState() => _DropDownRegisterState();
 }
 
 class _DropDownRegisterState extends State<DropDownRegister> {
-  String dropdDownValue;
+  String dropDown;
   @override
   Widget build(BuildContext context) {
     return     Container(
        width: 340,
        height: 60,
        child: DropdownButton<String>(
-        value: dropdDownValue,
-        hint: Text("Pregunta 1"),
+        value: dropDown,
+        hint: Text(widget.titleDropHint),
       /*   disabledHint: Text("Pregunta 1"), */
         
        // icon: const Icon(Icons.arrow_downward),
@@ -36,11 +37,7 @@ class _DropDownRegisterState extends State<DropDownRegister> {
 
           color: Color.fromRGBO(170, 195, 238, 1),
         ),
-        onChanged: (String newValue) {
-          setState(() {
-            dropdDownValue = newValue;
-          });
-        },
+        onChanged: widget.onChanged,
         items: <String>[widget.opcion1, widget.opcion2, widget.opcion3, widget.opcion4]
             .map<DropdownMenuItem<String>>((String value) {
 
